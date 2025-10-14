b3c7534e0788e531ca408c41c26cf7c464bcb021
Takuya Murakami <tmurakam@tmurakam.org>
2025-10-15 00:49:39 +0900

    chore: update do-kubespray.sh

--- do-kubespray.sh
+++ do-kubespray.sh
@@ -6,8 +6,11 @@ if [ ! -e $KUBESPRAY_DIR ]; then
     echo "No kubespray dir"
     exit 1
 fi
-cd $KUBESPRAY_DIR
+pushd $KUBESPRAY_DIR
 
 # Run kubespray
 export ANSIBLE_PYTHON_INTERPRETER=/usr/bin/python3
-ansible-playbook -i inventory/mycluster/hosts.yaml --become --become-user=root -u $SSH_USER cluster.yml $*
+ansible-playbook -i inventory/mycluster/hosts.yaml --become --become-user=root -u $SSH_USER cluster.yml $* || exit 1
+
+popd
+./setup-kubeconfig.sh
